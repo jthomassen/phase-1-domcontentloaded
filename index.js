a930d3ab835f6fa084e8e80d2fa1a699da00b38d
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-07-26 14:56:33 -0600

    commit tree da044eae785c3b05c853f532ff0aafc743ab35f9

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // console.log("The DOM has loaded");
-    // let text = document.querySelector('p#text')
-    // return text.innerText = 'This is really cool!'
-    updateDOM();
+    console.log("The DOM has loaded");
+    let text = document.querySelector('p#text')
+    return text.textContent = 'This is really cool!'
+    // updateDOM();
   }); 
 
-function updateDOM(){
-    document.getElementById('text')
-    .innerHTML = "This is really cool!"
-}
+// function updateDOM(){
+//     document.getElementById('text')
+//     .innerText = "This is really cool!"
+// }
 // console.log(
 //     "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
 // ); 
